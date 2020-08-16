5f5847b6cd26f7d646ac0dfd7b80bdf5917161dc
박성윤 <bagseong-yun@bagseong-yun-ui-MacBookPro.local>
2020-08-17 05:06:42 +0900

    Updates

---
+++ precache-manifest.11a627646c5c15a52e116edc66f3edf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd054786ec342b9663d1cf82332f06ca",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "0bccbf06240f0146451a",
+    "url": "/movie_app_2020/static/css/main.48978d63.chunk.css"
+  },
+  {
+    "revision": "38c19f6b71c12f2adda3",
+    "url": "/movie_app_2020/static/js/2.afd4ff74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.afd4ff74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bccbf06240f0146451a",
+    "url": "/movie_app_2020/static/js/main.7e08d515.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
